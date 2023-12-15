25b3196dbb96a3b45d60f81527fcb5f3a203e5d2
pr111rybakov <pr111rybakov@1-510-CLS-09>
2023-12-15 09:46:25 +0600

    Добавьте файлы проекта.

---
+++ WpfApp1/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ShortInfo { get; set; }
+        public Color PreferredColor { get; set; }
+        public ObservableCollection<string> Tariff { get; set; }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var list = new ObservableCollection<Client>()
+               {
+                new Client(){Id=1,FirstName="Син",LastName="Карсарович", PreferredColor=Colors.Beige, ShortInfo="Умею создавать динамит",
+                    Tariff = new ObservableCollection<string>()
+                {
+                    "Граната",
+                    "Мина"
+                } },
+                new Client(){Id=2,FirstName="Василий",LastName="Кисилив", PreferredColor=Colors.Azure, ShortInfo="Умею кататься на скутере", 
+                    Tariff = new ObservableCollection<string>()
+                {
+                    "Линейка",
+                    "Бокосины"
+                } },
+                new Client(){Id=3,FirstName="Константин",LastName="Чукчин", PreferredColor=Colors.BlanchedAlmond, ShortInfo="Сломал 10 деревьев", 
+                    Tariff = new ObservableCollection<string>()
+                {
+                    "Топор",
+                    "Бензопила"
+                } },
+                new Client(){Id=4,FirstName="Сергей",LastName="Кораблев", PreferredColor=Colors.Gainsboro, ShortInfo="Разработчик самолетов",
+                    Tariff =  new ObservableCollection<string>()
+                {
+
+                    "Вертолет",
+                    "Велик"
+                } }
+            };
+            ClientsList.ItemsSource = list;
+        }
+    }
+}
